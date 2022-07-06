b0bcb34587935eb2cf1f8f498c5ed8e98f13dd78
thiagocanali <thi.98.cwb@hotmail.com>
2022-07-06 15:55:59 -0300

    subindo passos 9,10,11,12,13,14,15,16,17 ✔️

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:flutter_crud/views/user_form.dart';
 import 'package:flutter_crud/views/user_list.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Users(),
+        )
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter CRUD - Lista de Usuarios',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          AppRoutes.HOME: (_) => UserList(),
+          AppRoutes.USER_FORM: (_) => UserForm()
+        },
       ),
-      home: UserList(),
     );
   }
 }
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user_tile.dart';
-import 'package:flutter_crud/data/dummy_users.dart';
 import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
-class UserList extends StatelessWidget {
+
+class  UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    final Users users = Provider.of(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Lista de Usuários'),
         actions: <Widget>[
-        IconButton(
+          IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-
+              Navigator.of(context).pushNamed(
+                AppRoutes.USER_FORM
+              );
             },
-          )
+
+          ),
         ],
       ),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
-      ),
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
+        ),
     );
   }
 }
